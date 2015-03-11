9146087f8a0dfd7353ccc98fa15e3a7b9e64ea08
rahul <rahul.yadav@hotelogix.com>
2015-03-11 16:49:02 +0530

    fst_commit

---
+++ src/test/java/hms/hot/hot/AppTest.java
@@ -0,0 +1,44 @@
+package hms.hot.hot;
+
+import static junit.framework.Assert.assertTrue;
+import java.io.IOException;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.WebDriverWait;
+ 
+public class AppTest {
+     private WebDriver driver;
+     private WebDriverWait wait;
+    
+    
+      @Before
+      public void setUp() {
+        driver = new FirefoxDriver();
+        wait = new WebDriverWait(driver, 10);
+        driver.get("http://www.google.com");
+      }
+      @After
+      public void tearDown() throws IOException {
+        driver.quit();
+      } 
+ 
+      @Test
+      public void pageTitleAfterSearchShouldBeginWithDrupal() throws IOException {
+        WebElement searchField = driver.findElement(By.name("q"));
+        searchField.sendKeys("Drupal!");
+        searchField.submit();
+//        assertTrue("The page title should start with the search string after the search.",
+//            wait.until(new ExpectedCondition() {
+//              public Boolean apply(WebDriver d) {
+//                return d.getTitle().toLowerCase().startsWith("drupal!");
+//              }
+//            }));
+      }
+    
+}
